86fe8654fbb70b07304026c1f28d4c7e7fc4f0e1
Dowland Aiello <mitsukomegumii@gmail.com>
2019-11-27 18:13:11 -0500

    Revert "Revert "notedly: Batch commit.""
    This reverts commit 30e40d23968cc46187a7c8873dc0d34fc1b122a0.

--- modules/web/server/src/note.rs
+++ modules/web/server/src/note.rs
@@ -1,28 +1,55 @@
-use mode::{Mode, geometric::Shape};
+use super::modes::{geometric::Shape, Mode};
 
-/// A string containing one or more characters.
+/// A character or sequence of characters.
 pub struct Expression {
-    /// The characters contained in the note
-    pub chars: Vec<char>, 
+    /// The expressions contained in the expression
+    pub sub_expressions: Option<Vec<Expression>>,
 
-    /// The points contained in a possibly geometric expression
-    pub shapes: Optin(Vec<Shape>),
+    /// The character contained in the expression
+    pub value: Option<char>,
 
-    /// The format in which the characters are written
+    /// The shape contained in the expression
+    pub shape: Option<Shape>,
+
+    /// The format in which the character is written
     pub mode: Mode,
+}
+
+impl Expression {
+    /// Initializes and returns a new expression with the given mode.
+    pub fn new(mode: Mode) -> Expression {
+        Expression{
+            sub_expression: None,
+            value: None,
+            shape: None,
+            mode: mode,
+        } // Return the new expression
+    }
 }
 
 /// A note on a given board, authored by a particular user.
 pub struct Note {
     /// The author of the note
-    author: String,
+    pub author: String,
 
     /// The last saved contents of the note
-    contents: Vec<Expression>,
+    pub contents: Vec<Expression>,
 
     /// Any notes contained inside the note
-    children: Vec<Node>,
+    pub children: Vec<Note>,
 
-    /// The dimensions of the note
-    dimensions: (i8, i8),
+    /// The dimensions of the note, as a percentage of the viewport
+    pub dimensions: (i8, i8),
+}
+
+impl Note {
+    /// Initializes and returns a new note with the given author.
+    pub fn new(author: String) -> Note {
+        Note {
+            author: author,
+            contents: Vec::new(),
+            children: Vec::new(),
+            dimensions: (10, 10),
+        } // Return the new note
+    }
 }
