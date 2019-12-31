428d9d4e453b1969507883ca688e152f9dc12493
Dowland Aiello <mitsukomegumii@gmail.com>
2019-12-31 15:49:59 -0500

    api/boards: Finish writing has_permissions helper method.
    Signed-off-by: Dowland Aiello <mitsukomegumii@gmail.com>

--- modules/web/server/src/api/boards.rs
+++ modules/web/server/src/api/boards.rs
@@ -16,6 +16,71 @@ use diesel::{
     BelongingToDsl, BoolExpressionMethods, ExpressionMethods, QueryDsl, RunQueryDsl,
 };
 
+/// Ensures that the user has the given permissions.
+fn has_permissions(
+    conn: PgConnection,
+    board_uid: i32,
+    user: User,
+    owner: bool,
+    read: bool,
+    write: bool,
+) -> Result<(), Error> {
+    // Construct a query to get the board with ID from the db
+    let b_query = boards.find(board_uid);
+
+    // The board must exist for all to continue. Otherwise, throw a 404.
+    if !exists(b_query).get_result(&conn)? {
+        // Return a 404
+        return Err(Error(error::ErrorNotFound()));
+    }
+
+    // Get the board that matches the given UID from the database
+    let matching_board = b_query.first(&conn)?;
+
+    // If the end-user wants to enforce that the token matches an owner, so be it
+    if owner {
+        // If the user is not registered as the owner, return an error.
+        if matching_boarduser_id != user.id {
+            // Respond with an unauth
+            return Err(Error(error::ErrorUnauthorized()));
+        }
+
+        // The user is authenticated
+        return Ok(());
+    }
+
+    // Construct a query to get the permission belonging to the user with the board
+    let p_query =
+        Permission::belonging_to(&user).filter(schema::permissions::board_id.eq(board_uid));
+
+    // If this permission doesn't exist, the user isn't even envited to the board
+    if !exists(p_query).get_result(&conn)? {
+        // Return a 404
+        return Err(Error(error::ErrorNotFound()));
+    }
+
+    // Get the permission belonging to the user
+    let permission: Permission = Permission::belonging_to(&user)
+        .filter(schema::permissions::board_id.eq(board_uid))
+        .first(&conn)?;
+
+    // Ensure the user has the proper permissions to be able to write & read to the file
+    if write && !permission.write || write && !permissions.read {
+        // Respond with an unauth
+        return Err(Error(error::ErrorUnauthorized()));
+    }
+
+    // Ensure that the user has the proper permissions to be able to read to the file
+    if read && !permissions.read {
+        // Respond with an unauth
+        return Err(Error(error::ErrorUnauthorized()));
+    }
+
+    // The user should only have gotten this far if each of the preconditions were met--meaning
+    // that the user does have sufficient privileges
+    Ok(())
+}
+
 /// Gets a list of board IDs that the currently authenticated user is able to view.
 ///
 /// # Arguments
@@ -322,4 +387,43 @@ pub async fn all_notes(
             .select(schema::notes::id)
             .get_results(&conn)?,
     ))
+}
+
+/// Gets a list of users associated with the board.
+///
+/// # Arguments
+///
+/// * `pool` - The connection pool that will be used to connect to the postgres database
+/// * `board_uid` - The ID of the requested board
+/// * `req` - An HTTP request provided by the caller of this method. Used to obtain the bearer
+/// token (required) of the user
+pub async fn all_users(
+    pool: Data<Pool<ConnectionManager<PgConnection>>>,
+    board_uid: Path<i32>,
+    req: HttpRequest,
+) -> Result<Json<Vec<i32>>, Error> {
+    // Get a connection from the provided connection pool, so we can start using dieisel
+    let conn = pool.get()?;
+
+    // Get an authorization token from the headers sent with the request
+    let token = extract_bearer(&req)?;
+
+    // Look at the request path, extract the board ID, and find the matching board.
+    let matching_board: Board = boards.find(*board_uid).first(&conn)?;
+
+    // Get the user from the database with the provided oauth token
+    let matching_user: User = users
+        .filter(schema::users::oauth_token.eq(hash_token(token)))
+        .first(&conn)?;
+
+    // Ensure that this user is capable of reading from this board
+    if !exists(
+        Permission::belonging_to(&matching_board).filter(
+            schema::permissions::user_id
+                .eq(matching_user.id)
+                .and(schema::permissions::read.eq(true)),
+        ),
+    ) {
+        return Err(Error(error::ErrorUnauthorized("The provided bearer token does not match a user with the required permissions to view this board.")));
+    }
 }
