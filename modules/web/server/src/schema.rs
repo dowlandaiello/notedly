d219eab84331272bd3c51c104e09a5b30bbf393b
Dowland Aiello <mitsukomegumii@gmail.com>
2020-01-07 12:50:39 -0500

    server: Run cargo fmt.
    Signed-off-by: Dowland Aiello <mitsukomegumii@gmail.com>

--- modules/web/server/src/schema.rs
+++ modules/web/server/src/schema.rs
@@ -36,9 +36,4 @@ table! {
     }
 }
 
-allow_tables_to_appear_in_same_query!(
-    boards,
-    notes,
-    permissions,
-    users,
-);
+allow_tables_to_appear_in_same_query!(boards, notes, permissions, users,);
