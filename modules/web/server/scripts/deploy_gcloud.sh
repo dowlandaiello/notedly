34dcdeb80a60726ff57a9d5535bdb6f6ad0f820b
Dowland Aiello <mitsukomegumii@gmail.com>
2020-04-12 12:28:50 -0400

    scripts: Update gcloud deployment script.
    Signed-off-by: Dowland Aiello <mitsukomegumii@gmail.com>

--- modules/web/server/scripts/deploy_gcloud.sh
+++ modules/web/server/scripts/deploy_gcloud.sh
@@ -4,5 +4,8 @@
 # moved to a directory where we should find one
 [ -f Dockerfile ] || cd ..
 
+# Remove an existing cluster
+kubectl delete -f ../../../deployments/api.yaml
+
 # Spawn a cluster
 kubectl create -f ../../../deployments/api.yaml
